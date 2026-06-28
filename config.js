8fb17b0517c369ee574b11fb4a66ee6ce96eb19f
Sandes <sandesissu7@gmail.com>
2026-06-28 14:53:12 +0530

    Update SESSION_ID to a new default value

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YRFXXKyT#xq9jBIrkaCJQWp_GYoxOUwvulxR0jSgdOXK4SXBqFdU",
+SESSION_ID: process.env.SESSION_ID || "xUUCzYDa#wnA4plfsiNLlE8xL3yehuYxdAHv9wmYNdMQ_x6cn8Dw",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
