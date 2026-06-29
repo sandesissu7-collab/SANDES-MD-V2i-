68f4d61a157077456d82f9f5e30cc36d89949dde
Sandes <sandesissu7@gmail.com>
2026-06-29 18:46:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function downloadAndExtractZip() {
             fs.mkdirSync(LIB_DIR, { recursive: true }); }
             console.log('\x1b[3m%s\x1b[0m', 'FETCHING ZIP FILES FROM mega.nz 💢...');
 
-        let MEGA_ZIP_LINK = String("https://mega.nz/file/lJknkRJJ#KXvcezeNIEorzx3Tli6H1hVQjYnptmEvKHbncyzR-_I").trim(); 
+        let MEGA_ZIP_LINK = String("https://mega.nz/file/kcMFBYZL#yAIBsLF_iTLn9MedvWWrv_yHRxEG4IJS9YAOENYmzPo").trim(); 
         if (!MEGA_ZIP_LINK.includes('#')) 
                        {
         throw new Error("MEGA link missing hash! Check zip.json"); }
